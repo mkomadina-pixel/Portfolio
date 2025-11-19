ca7886b427f4ac42a6920984dfc05a4d67850cc5
mkomadina-pixel <mkomadina@gaming.tech>
2025-11-19 13:10:22 +0100

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById('navbar');
+let lastScrollY = window.scrollY;
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 60) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
